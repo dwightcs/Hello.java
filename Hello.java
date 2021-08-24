dbdaaef54119400c6ef04cd025b9b481fd25ac91
Dwight Baldwin <87383264+dwightcs@users.noreply.github.com>
2021-08-24 13:46:28 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+/* First Java Program
+Dwight Baldwin Jr.
+August 24, 2021
+CMSC 255 Section 004 */
+
+public class Hello{
+	public static void main(String[] args) {
+	System.out.println("Dwight Baldwin Jr.");
+	System.out.println("First Java Program");
+	System.out.println("Making sure Java is working correctly on my Mac.");
+	System.out.println("August 24, 2021");
+	System.out.println("CMSC 255 Sec 004");
+	System.out.println();
+	}
+}
